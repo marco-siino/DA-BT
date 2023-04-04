f7e38d79c2c562a52fc2de0457875b66d4010869
Marco Siino <86612116+marco-siino@users.noreply.github.com>
2023-04-04 10:26:31 +0200

    Update dataset.py

--- code/dataset.py
+++ code/dataset.py
@@ -37,7 +37,7 @@ class Dataset:
           self.test_folder_name = "pan20-author-profiling-test-2020-02-23-"+self.language
         elif ds_name == 'hss':
           self.train_folder_name = "pan21-author-profiling-training-2021-03-14-"+self.language
-          self.test_folder_name = "pan21-author-profiling-test-2021-03-14-"+self.language
+          self.test_folder_name = "pan21-author-profiling-test-without-gold-"+self.language
         elif ds_name == 'iss':
           self.train_folder_name = "pan22-author-profiling-training-2022-03-29-"+self.language
           self.test_folder_name = "pan22-author-profiling-test-2022-04-22-"+self.language
